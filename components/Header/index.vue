d177c21e671490d80f084f00d980bbeebd0281dd
SugarShootingStar <ryu.8224@gmail.com>
2018-11-22 20:31:08 +0900

    Init + Header実装

---
+++ components/Header/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="header">
+    <ul class="header__list">
+      <li
+        v-for="(value, index) in links"
+        :key="index" 
+        class="header__list__item">
+        <vs-button
+          :color="value.color"
+          :type="value.type">
+          {{ value.name }}
+        </vs-button>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: 'ホーム',
+          color: 'primary',
+          type: 'gradient',
+          link: '/'
+        },
+        {
+          name: 'プロフィール',
+          color: 'success',
+          type: 'gradient',
+          link: '/profile'
+        },
+        {
+          name: 'スキル',
+          color: 'dark',
+          type: 'gradient',
+          link: '/skill'
+        },
+        {
+          name: '経歴',
+          color: 'danger',
+          type: 'gradient',
+          link: '/history'
+        },
+        {
+          name: '工事中',
+          color: 'warning',
+          type: 'gradient',
+          link: '/questions'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="~/components/Header/style.scss"/>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+    <Header/>
+    <section class="home">
+      <!-- <marquee 
+        behavior="scroll" 
+        direction="left"
+        class="home__title">ホームページ</marquee> -->
+    </section>
+  </article>
+</template>
+
+<script>
+import Header from '@/components/Header/index.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss" src="~/pages/style.scss"/>
