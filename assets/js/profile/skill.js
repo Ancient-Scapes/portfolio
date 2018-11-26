938bb7a7e714f731b8b8d976f26937b0f3eadef7
SugarShootingStar <ryu.8224@gmail.com>
2018-11-26 12:06:11 +0900

    feat:プロフィール周りを少し進めた

---
+++ assets/js/profile/skill.js
@@ -0,0 +1,34 @@
+export default {
+  language: ['JavaScript', 'Node.js', 'PHP', 'C#', 'Elixir'],
+  framework: {
+    javaScript: ['Vue.js', 'Nuxt.js', 'React.js', 'AngularJS', 'jQuery'],
+    nodeJs: ['express'],
+    php: ['FuelPHP', 'Laravel'],
+    elixir: ['Phoenix']
+  },
+  javaScriptTools: {
+    language: ['ES6', 'CSS/SASS'],
+    build: ['webpack', 'gulp', 'parcel', 'npm scripts'],
+    test: ['Jest', 'AVA'],
+    performance: ['ESLint', 'Prettier']
+  },
+  design: {
+    language: ['CSS', 'SASS/SCSS'],
+    developmentMethods: ['flexbox', 'BEM'],
+    tools: ['figma', 'Zeplin']
+  },
+  db: ['MySQL', 'OracleDB', 'Redis', 'SQL Server'],
+  os: ['Linux', 'Mac OSX', 'Windows'],
+  tools: [
+    'Unity',
+    'Git',
+    'Github',
+    'GitLab',
+    'Redmine',
+    'AWS EC2v',
+    'Firebase',
+    'Netlify',
+    'Docker'
+  ],
+  developmentMethods: ['ウォーターフォール', 'Git Flow', 'Github Flow']
+}
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <section class="container"/>
+  <section class="profile">
+    <!-- 使える言語、技術など -->
+    <div class="profile--skill"></div>
+    <!-- 業務経歴など -->
+    <div class="profile--history"></div>
+    <!-- 成果物など -->
+    <div class="profile--works"></div>
+  </section>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss" src="~/pages/style.scss"/>
+<style lang="scss" src="~/pages/profile/style.scss"/>
