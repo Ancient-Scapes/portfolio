603fe1c78229aa1c43f555c1e8cce7f5d9709f66
SugarShootingStar <ryu.8224@gmail.com>
2018-11-27 01:55:11 +0900

    feat:Imageコンポーネント追加

---
+++ components/Image/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="image__wrapper">
+    <img 
+      :src="src" 
+      class="image__main">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="~/components/Image/style.scss"/>
