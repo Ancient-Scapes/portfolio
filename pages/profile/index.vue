2a96078592937dd4f255ccbf578b01edb21d8657
SugarShootingStar <ryu.8224@gmail.com>
2018-11-27 01:55:53 +0900

    feat:プロフィールの言語一旦出してみたところまで

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -7,6 +7,7 @@
     <!-- 使える言語、技術など -->
     <div class="profile--skill">
       <div class="profile--skill__language">
+        <!-- 言語 -->
         <vs-list class="profile--skill__language__list">
           <vs-list-header 
             icon="supervisor_account" 
@@ -14,24 +15,26 @@
           <vs-list-item
             v-for="(value, index) in skill.language"
             :key="index"
-            :title="value"
+            :title="value.name"
             class="profile--skill__language__list__item">
+            <!-- 言語アイコン -->
             <template slot="avatar">
-              <div class="profile--skill__language__list__item__wrapper">
-                <img 
-                  :src="`/img/profile/language/${value}.png`"
-                  class="profile--skill__language__list__item__icon">
-              </div>
+              <CommonImage :src="`/img/profile/language/${value.name}.png`"/>
             </template>
+
+            <!-- フレームワークなどのアイコン -->
+            <div class="profile--skill__language__list__item__sub">
+              <ImageList :src="srcFramework(value.framework)"/>
+              <!-- TODO 画像を複数横並びにするコンポーネント -->
+            </div>
           </vs-list-item>
         </vs-list>
       </div>
-      <!-- <div class="profile--skill__framework"></div>
-      <div class="profile--skill__javaScriptTools"></div>
+      <!-- <div class="profile--skill__javaScriptTools"></div>
       <div class="profile--skill__design"></div>
       <div class="profile--skill__db"></div>
       <div class="profile--skill__os"></div>
-      <div class="profile--skill__tools"></div> -->
+      <div class="profile--skill__other"></div> -->
     </div>
     <!-- 業務経歴など -->
     <!-- <div class="profile--history"></div> -->
@@ -42,11 +45,24 @@
 
 <script>
 import skill from '~/assets/js/profile/skill.js'
+import CommonImage from '~/components/Image/index.vue'
+import ImageList from '~/components/ImageList/index.vue'
 
 export default {
+  components: {
+    CommonImage,
+    ImageList
+  },
   data() {
     return {
       skill
+    }
+  },
+  methods: {
+    srcFramework(framework) {
+      return Array.prototype.map.call(Object(framework), value => {
+        return `/img/profile/framework/${value}.png`
+      })
     }
   }
 }
