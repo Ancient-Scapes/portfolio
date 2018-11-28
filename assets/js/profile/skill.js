0a904cb7dbe729b96537be159376f174c0fdd224
SugarShootingStar <ryu.8224@gmail.com>
2018-11-28 12:29:16 +0900

    fix:画像ディレクトリ変更に伴い構成を変更

--- assets/js/profile/skill.js
+++ assets/js/profile/skill.js
@@ -1,63 +1,119 @@
 export default {
   language: {
-    javaScript: {
-      name: 'JavaScript',
-      item: ['Vue.js', 'Nuxt.js', 'React.js', 'AngularJS', 'jQuery']
-    },
-    nodeJs: {
-      name: 'Node.js'
-    },
-    css: {
-      name: 'CSS'
-    },
-    sass: {
-      name: 'Sass'
-    },
-    php: {
-      name: 'PHP',
-      item: ['FuelPHP', 'Laravel']
-    },
-    cSharp: {
-      name: 'C Sharp',
-      item: ['dotNET', 'Unity']
-    },
-    elixir: {
-      name: 'Elixir',
-      item: ['Phoenix']
+    title: '言語',
+    icon: 'text_format',
+    item: {
+      javaScript: {
+        name: 'JavaScript',
+        item: ['Vue.js', 'Nuxt.js', 'React.js', 'AngularJS', 'jQuery']
+      },
+      nodeJs: {
+        name: 'Node.js'
+      },
+      css: {
+        name: 'CSS'
+      },
+      sass: {
+        name: 'Sass'
+      },
+      php: {
+        name: 'PHP',
+        item: ['FuelPHP', 'Laravel']
+      },
+      cSharp: {
+        name: 'C Sharp',
+        item: ['dotNET', 'Unity']
+      },
+      elixir: {
+        name: 'Elixir',
+        item: ['Phoenix']
+      }
     }
   },
   javaScriptTools: {
-    build: {
-      name: 'ビルドツール、タスクランナー',
-      item: ['webpack', 'gulp', 'parcel', 'npm scripts']
-    },
-    test: {
-      name: 'テスト',
-      item: ['Jest', 'AVA']
-    },
-    formatter: {
-      name: 'フォーマッター',
-      item: ['ESLint', 'Prettier']
+    title: 'JavaScript周辺技術',
+    icon: 'build',
+    item: {
+      build: {
+        name: 'ビルドツール、タスクランナー',
+        item: ['webpack', 'gulp', 'parcel', 'npm scripts']
+      },
+      test: {
+        name: 'テスト',
+        item: ['Jest', 'AVA']
+      },
+      formatter: {
+        name: 'フォーマッター',
+        item: ['ESLint', 'Prettier']
+      }
     }
   },
   db: {
-    name: 'データベース',
-    item: ['MySQL', 'OracleDB', 'redis', 'SQL Server']
+    title: 'データベース',
+    icon: 'build',
+    item: {
+      rdbms: {
+        name: 'RDBMS',
+        item: ['MySQL', 'OracleDB', 'SQL Server']
+      },
+      nosql: {
+        name: 'NoSQL',
+        item: ['redis']
+      }
+    }
   },
   os: {
-    name: 'OS',
-    item: ['Linux', 'Mac OSX', 'Windows']
-  },
-  editor: {
-    name: '開発エディタ',
-    item: ['VSCode']
+    title: 'OS',
+    icon: 'build',
+    item: {
+      widnows: {
+        name: 'Windows',
+        item: ['Windows10']
+      },
+      mac: {
+        name: 'Mac',
+        item: ['Mac OSX']
+      },
+      linux: {
+        name: 'Linux系',
+        item: ['CentOS']
+      }
+    }
   },
   project: {
-    name: 'プロジェクト管理',
-    item: ['Github', 'GitLab', 'Redmine']
+    title: 'プロジェクト管理',
+    icon: 'build',
+    item: {
+      git: {
+        name: 'git系',
+        item: ['Github', 'GitLab']
+      },
+      task: {
+        name: 'タスク管理',
+        item: ['Redmione']
+      }
+    }
   },
   infra: {
-    name: 'インフラ関係など',
-    item: ['AWS EC2', 'Firebase', 'Netlify', 'docker']
+    title: 'インフラ関係など',
+    icon: 'build',
+    item: {
+      aws: {
+        name: 'AWS',
+        item: ['EC2']
+      },
+      firebase: {
+        name: 'Firebase',
+        item: ['Cloud Functions']
+      },
+      deploy: {
+        name: 'デプロイ',
+        item: ['Netlify']
+      },
+      docker: {
+        name: 'docker',
+        item: ['docker-compose']
+      }
+    }
   }
 }
