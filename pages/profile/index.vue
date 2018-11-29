6aa0e9dd98501eaf947cdf6c87ca6a636f17d46d
SugarShootingStar <ryu.8224@gmail.com>
2018-11-29 14:41:54 +0900

    feat:プロフィールページそこそこできてきた

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -6,35 +6,15 @@
 
     <!-- 使える言語、技術など -->
     <div class="profile--skill">
-      <div class="profile--skill__language">
-        <!-- 言語 -->
-        <vs-list class="profile--skill__language__list">
-          <vs-list-header 
-            icon="supervisor_account" 
-            title="言語"/>
-          <vs-list-item
-            v-for="(value, index) in skill.language"
-            :key="index"
-            :title="value.name"
-            class="profile--skill__language__list__item">
-            <!-- 言語アイコン -->
-            <template slot="avatar">
-              <CommonImage :src="`/img/profile/language/${value.name}.png`"/>
-            </template>
 
-            <!-- フレームワークなどのアイコン -->
-            <div class="profile--skill__language__list__item__sub">
-              <ImageList :src="srcFramework(value.framework)"/>
-              <!-- TODO 画像を複数横並びにするコンポーネント -->
-            </div>
-          </vs-list-item>
-        </vs-list>
-      </div>
-      <!-- <div class="profile--skill__javaScriptTools"></div>
-      <div class="profile--skill__design"></div>
-      <div class="profile--skill__db"></div>
-      <div class="profile--skill__os"></div>
-      <div class="profile--skill__other"></div> -->
+      <skillList
+        v-for="(value, key) in skill"
+        :key="key"
+        :data="value.item"
+        :list-title="value.title"
+        :list-icon="value.icon"
+        :img-dir-name="key"/>
+
     </div>
     <!-- 業務経歴など -->
     <!-- <div class="profile--history"></div> -->
@@ -47,11 +27,13 @@
 import skill from '~/assets/js/profile/skill.js'
 import CommonImage from '~/components/Image/index.vue'
 import ImageList from '~/components/ImageList/index.vue'
+import SkillList from '~/components/List/Skill/index.vue'
 
 export default {
   components: {
     CommonImage,
-    ImageList
+    ImageList,
+    SkillList
   },
   data() {
     return {
@@ -59,9 +41,9 @@ export default {
     }
   },
   methods: {
-    srcFramework(framework) {
-      return Array.prototype.map.call(Object(framework), value => {
-        return `/img/profile/framework/${value}.png`
+    getSrc(arr, dirName) {
+      return Array.prototype.map.call(Object(arr), value => {
+        return `/img/profile/${dirName}/${value}.png`
       })
     }
   }
