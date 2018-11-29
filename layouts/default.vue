e72ef7a727f5dde0c39f41491afd225d79ed9bb4
SugarShootingStar <ryu.8224@gmail.com>
2018-11-29 20:52:14 +0900

    feat:defaultのレイアウトにFooter追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>
-    <Header class="header"/>
-    <div class="main">
+  <div class="default">
+    <Header class="default--header"/>
+    <div class="default--main">
       <nuxt/>
     </div>
+    <Footer class="default--footer"/>
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header'
+import Footer from '~/components/Footer'
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
-
 
 <style lang="scss" src="./style.scss"/>
