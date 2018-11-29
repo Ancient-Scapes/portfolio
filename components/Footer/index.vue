0c9ea4823ada9c2c452f9f48d2145aab0463705e
SugarShootingStar <ryu.8224@gmail.com>
2018-11-29 20:51:48 +0900

    feat:Footer追加

---
+++ components/Footer/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="footer">
+    <div class="footer__copyright">
+      <span class="footer__copyright__text">
+        ©SugarShootingStar
+      </span>
+    </div>
+
+    <div class="footer__mode">
+      <vs-chip>
+        <vs-avatar
+          icon="thumb_up"
+          @click="$store.commit('changeMode')"/>
+        {{ $store.getters.getMode }}
+      </vs-chip>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style lang="scss" src="./style.scss"/>
