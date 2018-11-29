1abeaa925d6de16bf2a8a55550558209c06f48ee
SugarShootingStar <ryu.8224@gmail.com>
2018-11-29 22:44:24 +0900

    feat:Iconコンポーネント追加

---
+++ components/Icon/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="icon">
+    <i class="material-icons">{{ name }}</i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss"/>
