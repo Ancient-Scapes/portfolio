90e65937367832e26e5afe54b8445331659a250c
SugarShootingStar <ryu.8224@gmail.com>
2018-11-30 18:53:11 +0900

    fix:ヘッダーがそこそこできた

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,50 +1,62 @@
 <template>
   <section class="header">
-    <ul
+    <!-- 古代版ヘッダ -->
+    <header 
       v-if="$store.getters.isOld"
-      class="header__list header__list__old">
-      <li
-        v-for="(value, index) in MenuContents.links"
-        :key="index" 
-        class="header__list__item header__list__item__old">
-        <MenuButton 
-          :text="value.name"
-          :link="value.link"/>
-      </li>
-    </ul>
-    <ul
+      class="header--header">
+      <ul class="header__list header__list__old">
+        <li
+          v-for="(value, index) in MenuContents.links"
+          :key="index" 
+          class="header__list__item">
+          <MenuButton 
+            :text="value.name"
+            :link="value.link"/>
+        </li>
+        <li><ModeChangeButton class="header__mode"/></li>
+      </ul>
+    </header>
+    <!-- 現代版ヘッダ -->
+    <div
       v-else-if="$store.getters.isFuture"
       class="header__list header__list__future">
-      <li
-        v-for="(value, index) in MenuContents.links"
-        :key="index" 
-        class="header__list__item header__list__item__future">
-        <vs-button>
-          <nuxt-link :to="value.link">
-            {{ value.name }}
-          </nuxt-link>
-        </vs-button>
-      </li>
-    </ul>
+      <vs-navbar>
+        <vs-avatar src="/img/profile/karipori.png"/>
 
-    <div class="header__mode">
-      <vs-chip>
-        <vs-avatar
-          icon="thumb_up"
-          @click="$store.commit('changeMode')"/>
-        {{ $store.getters.getMode }}
-      </vs-chip>
+        <vs-navbar-title>
+          <nuxt-link 
+            to="/"
+            class="header__list__future__title__text">
+            SyukeP's Homepage
+          </nuxt-link>
+        </vs-navbar-title>
+
+        <vs-spacer/>
+
+        <vs-navbar-item 
+          v-for="(value, index) in MenuContents.links"
+          :key="index" 
+          :index="index">
+          <nuxt-link :to="value.link">{{ value.nameEn }}</nuxt-link>
+        </vs-navbar-item>
+        <vs-navbar-item>
+          <ModeChangeButton class="header__mode"/>
+        </vs-navbar-item>
+      </vs-navbar>
     </div>
+
   </section>
 </template>
 
 <script>
 import MenuButton from '@/components/Button/Menu'
 import MenuContents from '~/assets/js/Components/Header/menu.js'
+import ModeChangeButton from '~/components/Button/ModeChange'
 
 export default {
   components: {
-    MenuButton
+    MenuButton,
+    ModeChangeButton
   },
   data() {
     return {
