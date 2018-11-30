f60dcd7b0903cc60fe562b26860d0c05dbba1986
SugarShootingStar <ryu.8224@gmail.com>
2018-11-30 18:45:50 +0900

    fix:メニューの英語文言追加

--- assets/js/Components/Header/menu.js
+++ assets/js/Components/Header/menu.js
@@ -2,18 +2,22 @@ export default {
   links: [
     {
       name: 'ホーム',
+      nameEn: 'Home',
       link: '/'
     },
     {
       name: 'プロフィール',
+      nameEn: 'Profile',
       link: '/profile'
     },
     {
       name: '作ったもの',
+      nameEn: 'Works',
       link: '/works'
     },
     {
       name: 'お問い合わせ',
+      nameEn: 'Contact',
       link: '/contact'
     }
   ]
