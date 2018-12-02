b0799ca91177b2c4b50ccc559705aafa79c2797f
SugarShootingStar <ryu.8224@gmail.com>
2018-12-02 18:25:37 +0900

    feat:particles.jsを導入して雪を降らせてみた

---
+++ components/Animation/SnowFall/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="snowfall"/>
+</template>
+
+<script>
+import snow from '~/assets/js/components/Animation/snow.js'
+
+export default {
+  data() {
+    return {
+      snow
+    }
+  },
+  mounted() {
+    require('particles.js')
+    window.particlesJS('snowfall', snow.data)
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss"/>
