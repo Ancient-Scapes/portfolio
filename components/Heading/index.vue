40b4525ef00571627c1823437cebb77bf4df41a8
SugarShootingStar <ryu.8224@gmail.com>
2018-12-03 11:32:59 +0900

    feat:見出し用コンポーネント実装

---
+++ components/Heading/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <TextFrame
+    :text="text"
+    :icon="icon"/> -->
+  <div class="heading">
+    <h2 class="heading__h2">
+      <Icon :name="icon"/> 
+      {{ text }}
+    </h2>
+  </div>
+</template>
+
+<script>
+import Icon from '~/components/Icon'
+
+export default {
+  components: {
+    Icon
+  },
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss"/>
