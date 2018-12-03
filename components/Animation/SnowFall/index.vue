7896394e357dcff13383e779572067099ed89800
SugarShootingStar <ryu.8224@gmail.com>
2018-12-03 20:19:45 +0900

    fix:dataに入れる必要のないものを削除

--- components/Animation/SnowFall/index.vue
+++ components/Animation/SnowFall/index.vue
@@ -6,11 +6,6 @@
 import snow from '~/assets/js/components/Animation/snow.js'
 
 export default {
-  data() {
-    return {
-      snow
-    }
-  },
   mounted() {
     window.particlesJS('snowfall', snow.data)
   }
