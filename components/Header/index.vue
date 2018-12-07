f330b387f4323e8a3dc7c272b26454db9bb45a18
SugarShootingStar <ryu.8224@gmail.com>
2018-12-07 15:47:47 +0900

    fix:ヘッダー周りのデザインを中心に修正

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -4,43 +4,45 @@
     <header 
       v-if="$store.getters.isOld"
       class="header--header">
-      <ul class="header__list header__list__old">
+      <ul class="header__list header__list--old">
+        <!-- ページリンク -->
         <li
           v-for="(value, index) in MenuContents.links"
           :key="index" 
-          class="header__list__item">
-          <MenuButton 
-            :text="value.name"
-            :link="value.link"/>
+          class="header__list--old__item">
+          <span class="header__list--old__item__text">
+            <nuxt-link :to="value.link">
+              {{ value.name }}
+            </nuxt-link>
+          </span>
         </li>
-        <li><ModeChangeButton class="header__mode"/></li>
+        <!-- モード変更ボタン -->
+        <li>
+          <ModeChangeButton class="header__list--old__mode"/>
+        </li>
       </ul>
     </header>
     <!-- 現代版ヘッダ -->
     <div
       v-else-if="$store.getters.isFuture"
-      class="header__list header__list__future">
+      class="header__list header__list--future">
       <vs-navbar>
+        <!-- プロフィール画像 -->
         <vs-avatar src="/img/profile/karipori.png"/>
 
-        <vs-navbar-title>
-          <nuxt-link 
-            to="/"
-            class="header__list__future__title__text">
-            SyukeP's Homepage
-          </nuxt-link>
-        </vs-navbar-title>
-
+        <!-- いい感じの余白 -->
         <vs-spacer/>
 
+        <!-- ページリンク -->
         <vs-navbar-item 
           v-for="(value, index) in MenuContents.links"
           :key="index" 
           :index="index">
           <nuxt-link :to="value.link">{{ value.nameEn }}</nuxt-link>
         </vs-navbar-item>
+        <!-- モード変更ボタン -->
         <vs-navbar-item>
-          <ModeChangeButton class="header__mode"/>
+          <ModeChangeButton class="header__list--future__mode"/>
         </vs-navbar-item>
       </vs-navbar>
     </div>
@@ -49,13 +51,11 @@
 </template>
 
 <script>
-import MenuButton from '@/components/Button/Menu'
 import MenuContents from '~/assets/js/Components/Header/menu.js'
 import ModeChangeButton from '~/components/Button/ModeChange'
 
 export default {
   components: {
-    MenuButton,
     ModeChangeButton
   },
   data() {
