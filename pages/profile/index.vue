37ea912f52787f2be21f7a4bbd6de0e7dba32f0b
SugarShootingStar <ryu.8224@gmail.com>
2018-12-19 18:11:01 +0900

    feat:プロフィールページ仮組み

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,13 +1,57 @@
 <template>
   <section class="profile">
-    <div class="profile--title">
-      プロフィールページ
+    <div class="profile__title">プロフィールページ</div>
+
+    <!-- ユーザアイコン -->
+    <div class="profile__top__icon">
+      <img 
+        src="img/profile/karipori.png"
+        class="profile__top__icon__image">
     </div>
+
+    <div class="profile__top__data">
+      <vs-list>
+        <vs-list-header
+          icon="build"
+          title="基本情報"/>
+        <vs-list-item
+          v-for="(value, index) in profile.info"
+          :key="index"
+          :title="value.caption"
+          :subtitle="value.value"
+          icon="build"/>
+      </vs-list>
+      <!-- 情報 -->
+      <div class="profile__top__data__links">
+        <ul class="profile__top__data__links__list">
+          <li
+            v-for="(value, index) in profile.links"
+            :key="index">
+            <vs-avatar :src="`img/profile/links/${value.title}.png`"/>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="profile__talking">
+      <vs-list>
+        <vs-list-header
+          icon="build"
+          title="登壇・LT歴"/>
+        <vs-list-item
+          v-for="(value, index) in profile.talking"
+          :key="index"
+          :title="value.title"
+          :subtitle="value.url"
+          icon="build"/>
+      </vs-list>
+    </div>
+
   </section>
 </template>
 
 <script>
-import skill from '~/assets/js/Pages/profile/skill.js'
+import profile from '~/assets/js/Pages/profile/profile'
 import CommonImage from '~/components/Image'
 
 export default {
@@ -16,7 +60,7 @@ export default {
   },
   data() {
     return {
-      skill
+      profile
     }
   }
 }
